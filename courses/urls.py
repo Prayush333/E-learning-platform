f5cd9b57ce0b81052362c0aa4f16b5d7b5ffcad9
Prayush Neupane <pyprayushneupane@gmail.com>
2025-05-24 21:00:52 +0545

    content form.html

--- courses/urls.py
+++ courses/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('create/', views.CourseCreateView.as_view(), name='course_create'),
     path('<pk>/edit/', views.CourseUpdateView, name='course_edit'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
-    path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update',)
+    path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+    path('module/<int:module_id>/content/<model_name>/create/', views.ContentCreateUpdateView.as_view(), name='module_content_create'),
+    path('module/<int:module_id>/content/<model_name>/<id>', name='module_content_update'),
+
 ]
